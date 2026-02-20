cbd819d588b2f2079772b3b91c6b52b00370dc85
VulcanWong <wongvulcan@gmail.com>
2026-02-20 23:27:33 +0800

    lesson46

---
+++ homework.py
@@ -0,0 +1,13 @@
+class expression:
+    emotions = ['happy', 'sad', 'angry', 'super angry', 'crazy']
+    def __init__(self):
+        print("happy")
+    def __init__(self):
+        print("sad")
+    def __init__(self):
+        print("angry")
+    def __init__(self):
+        print("super angry")
+    def __init__(self):
+        print("crazy")
+    print(emotions)
