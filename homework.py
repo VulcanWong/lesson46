a64506a58d479ac8d44784f826d44bf99fe153e2
VulcanWong <wongvulcan@gmail.com>
2026-02-21 20:31:08 +0800

    lesson46

--- homework.py
+++ homework.py
@@ -6,6 +6,7 @@ class expression:
         print(num1)
         print(num2)
         print(num3)
+        print(num1 + num2 + num3)
     number = ['num1', 'num2', 'num3']
     popitem = number.pop()
     print(popitem)
