0d2191315671f7fc0eaea4e596bb19acb8f79d30
VulcanWong <wongvulcan@gmail.com>
2026-02-21 19:58:20 +0800

    lesson46

--- homework.py
+++ homework.py
@@ -1,12 +1,10 @@
 class expression:
     def __init__(self):
         num1 = 1
-        print(num1)
-    def __init__(self):
         num2 = 2
-        print(num2)
-    def __init__(self):
         num3 = 3
+        print(num1)
+        print(num2)
         print(num3)
     number = ['num1', 'num2', 'num3']
     popitem = number.pop()
